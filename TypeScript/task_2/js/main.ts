d57fc2b3ea20adca3ffe8bcd833c9f15fbd95fcc
dosleam <9400@holbertonstudents.com>
2025-01-29 14:37:00 +0100

    fix

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,16 +1,77 @@
-// Définition du type littéral de chaîne Subjects
-type Subjects = "Math" | "History";
-
-// Fonction teachClass qui retourne une chaîne en fonction de todayClass
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === "Math") {
-    return "Teaching Math";
-  } else if (todayClass === "History") {
-    return "Teaching History";
-  }
-  return ""; // Cas de secours, mais ce ne devrait pas arriver avec le type Subjects
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-// Test de la fonction
-console.log(teachClass("Math"));     // Affiche "Teaching Math"
-console.log(teachClass("History"));  // Affiche "Teaching History"
+interface teacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+      return "Working from home";
+  }
+  getCoffeeBreak(): string {
+      return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+      return "Getting to director tasks";
+  }
+}
+
+class Teacher implements teacherInterface {
+  workFromHome(): string {
+      return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+      return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+      return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+      return new Teacher();
+  } else {
+      return new Director();
+  }
+}
+
+// Prédicat de type pour vérifier si l'employé est un Director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// Fonction executeWork
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Définir le type littéral pour les sujets
+type Subjects = "Math" | "History";
+
+// Fonction teachClass
+function teachClass(todayClass: Subjects): string {
+if (todayClass === "Math") {
+  return "Teaching Math";
+} else if (todayClass === "History") {
+  return "Teaching History";
+}
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
